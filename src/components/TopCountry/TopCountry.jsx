4adad6ad902228761a5d7442a35838942f5e74fd
sajjathossain <painlesssajjad@gmail.com>
2021-06-05 21:04:23 +0600

    Card ui changed

--- src/components/TopCountry/TopCountry.jsx
+++ src/components/TopCountry/TopCountry.jsx
@@ -4,7 +4,7 @@ import styles from './TopCountry.module.css'
 const TopCountry = () => {
   return (
     <div className={`${styles.countryContainer} applyBoxShadow`}>
-      <h1 className={styles.header}>Top Countries</h1>
+      {/* <h1 className={styles.header}>Top Countries</h1> */}
       <TabComponent className={styles.tabComponent}/>
     </div>
   )
